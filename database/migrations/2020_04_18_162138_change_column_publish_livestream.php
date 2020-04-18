7728234130dee7d70d27b6816ab2bd4703ddf14a
trantung <trantunghn196@gmail.com>
2020-04-18 18:12:10 +0700

    rename

--- database/migrations/2020_04_18_162138_change_column_publish_livestream.php
+++ database/migrations/2020_04_18_162138_change_column_publish_livestream.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeColumnNameLivestream extends Migration
+class ChangeColumnPublishLivestream extends Migration
 {
     /**
      * Run the migrations.
