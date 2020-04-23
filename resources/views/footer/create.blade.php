8235c6c58392e171219ef6a09ad3f38e9be840be
Nguyễn Văn Đăng <dangnv@edmicro.vn>
2020-04-23 22:11:36 +0700

    dangnv commit

--- resources/views/footer/create.blade.php
+++ resources/views/footer/create.blade.php
@@ -14,44 +14,38 @@
     </div>
     <div class="x_content">
       <br>
-      {{ Form::open(array('method'=>'POST','files'=>true, 'action' => array('FooterController@store'),'class'=>'form-horizontal form-label-left')) }}
+      {{ Form::open(array('method'=>'POST','files'=>true, 'action' => array('FooterController@store'),'class'=>'form-horizontal form_footer')) }}
       <h2>Thời gian hiển thị trong ngày</h2>
       <div class="form-group row">
         <div class="col-lg-6 col-md-12">
           <div class="col-lg-6">
-            <div class="multiselect_div">
-              <label>Từ giờ</label>
-              <div class="datetimepicker3">
-                <input data-format="hh:mm" type="text" name="start_time"></input>
-                <span class="add-on">
-                  <i data-time-icon="icon-time" data-date-icon="icon-calendar">
-                  </i>
-                </span>
-              </div>
+            <label>Từ giờ</label>
+            <div class="datetimepicker3">
+            <span class="add-on" id="icon_start_footer">
+                <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+                </i>
+              </span>
+              <input data-format="hh:mm" type="text" name="start_time" id="start_time" class="start_time_footer"></input>
             </div>
           </div>
           <div class="col-lg-6">
-            <div class="multiselect_div">
-              <label>Đến giờ</label>
-              <div class="datetimepicker4 " id="input-append">
-                <input data-format="hh:mm" type="text" name="end_time"></input>
-                <span class="add-on">
-                  <i data-time-icon="icon-time" data-date-icon="icon-calendar">
-                  </i>
-                </span>
-              </div>
+            <label class="col-lg-2">Đến giờ</label>
+            <div class="datetimepicker4 col-lg-10" id="input-append">
+              <span class="add-on" id="icon_end_footer">
+                <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+                </i>
+              </span>
+              <input data-format="hh:mm" type="text" name="end_time" id="end_time"></input>
             </div>
           </div>
         </div>
         <div class="col-lg-6 col-md-12">
             <label class="col-lg-2 col-md-2">trạng thái</label>
             <div class="col-md-10 col-lg-10">
-              <div class="multiselect_div">
                 <select class="form-control" name="status">
                   <option value="0">deactivate</option>
                   <option value="1">active</option>
                 </select>
-              </div>
             </div>
         </div>
       </div>
