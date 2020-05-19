3c3ed3fb4532ffedefabee522c52b68cba9bb95a
trantung <trantunghn196@gmail.com>
2020-05-19 22:17:12 +0700

    them require_login in livestream api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -105,6 +105,7 @@ class ApiController extends Controller
         $result['start_time'] = $startTimeFormat;
         $result['end_time'] = $endTime;
         $result['expire_date'] = $value->end_time;
+        $result['require_login'] = $value->require_login;
         $result['repeat'] = $value->repeat;
         $result['livestream_status'] = $status['livestream_status'];
         $result['livestream_status_name'] = $status['livestream_status_name'];
