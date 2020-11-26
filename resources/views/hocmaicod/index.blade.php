72d18788bb44d7dd2b828e27c4368a968231e40e
dangnv1994 <34670554+dangnv1994@users.noreply.github.com>
2020-11-26 21:23:16 +0700

    dangnv update index

--- resources/views/hocmaicod/index.blade.php
+++ resources/views/hocmaicod/index.blade.php
@@ -89,30 +89,3 @@
     </div>
   </div>
   @stop
-  @section('scripts')
-  <!-- <link href="{{asset('css/bootstrap-multiselect.css')}}" rel="stylesheet"/>
-  <link href="{{asset('css/jquery.dataTables.min.css')}}" rel="stylesheet" />
-  <link href="{{asset('css/buttons.dataTables.min.css')}}" rel="stylesheet" /> -->
-  <link href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet"/>
-  <link href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css"/>
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
-  <script>
-  $(document).ready(function() {
-    $('#example').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
-        ]
-    } );
-  } );
-  </script>
-  @stop
