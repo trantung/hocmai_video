882ed38d9492ab32a91c1bcace5fd5987c4ad0e0
trantung <trantunghn196@gmail.com>
2020-12-26 11:58:00 +0700

    them is_active va is_screen api danh sach event

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -575,6 +575,8 @@ class ApiController extends Controller
             $result[$key]['event_is_public'] = $value->is_public;
             $result[$key]['event_deep_link'] = $value->deep_link;
             $result[$key]['event_adjust'] = $value->event_adjust;
+            $result[$key]['event_adjust'] = $value->is_screen;
+            $result[$key]['event_adjust'] = $value->is_active;
         }
         return $this->responseSuccess($result);
     }
